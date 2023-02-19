5ab4f97690a41d6c77ac922c04f9dea6b3fd302a
Ajith Ashokkumar <ajith.ashokkumar.aj@gmail.com>
2023-02-19 15:49:31 +0000

    commiting updated pom.xml

--- appiumTests/src/test/java/TestRunner.java
+++ appiumTests/src/test/java/TestRunner.java
@@ -1,14 +1,14 @@
 
 
-import org.junit.runner.RunWith;
-import io.cucumber.junit.CucumberOptions;
-import io.cucumber.junit.Cucumber;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/Features/login.feature", glue= {"stepDefinitions"},
-
-plugin= {"pretty", "html:target/HtmlReports"})
-public class TestRunner  {
-}
+//import org.junit.runner.RunWith;
+//import io.cucumber.junit.CucumberOptions;
+//import io.cucumber.junit.Cucumber;
+//
+//@RunWith(Cucumber.class)
+//@CucumberOptions(features="src/test/resources/Features/login.feature", glue= {"stepDefinitions"},
+//
+//plugin= {"pretty", "html:target/HtmlReports"})
+//public class TestRunner  {
+//}
 
 
--- appiumTests/src/test/java/stepDefinitions/TestRunner.java
+++ appiumTests/src/test/java/stepDefinitions/TestRunner.java
@@ -4,11 +4,23 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.Cucumber;
 
+
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/Features/", glue= {"stepDefinitions"},
 
 plugin= {"pretty", "html:target/HtmlReports"})
+			
+
+//"io.qameta.allure.cucumberjvm.AllureCucumberJvm"})
+//plugin = { "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"})
 public class TestRunner  {
+	
+	
+//	public static void writeExtentReport() {
+//        Reporter.loadXMLConfig(new File("config/report.xml"));
 }
+
+
+
 
 
